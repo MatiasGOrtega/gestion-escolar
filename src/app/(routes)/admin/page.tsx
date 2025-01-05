061f92a2c9c2ddeba351b3b79596049e34193574
MatiasOrtega <mat-1998-ort@hotmail.com>
2025-01-04 21:44:28 -0300

    Add UserCard component and update AdminPage layout

--- src/app/(routes)/admin/page.tsx
+++ src/app/(routes)/admin/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import UserCard from "@/components/admin/UserCard";
+import React from "react";
 
 function AdminPage() {
   return (
-    <div>AdminPage</div>
-  )
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
