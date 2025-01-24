0266d63febdb584e8aaf5d5a0bbbab5739f79ce7
MatiasOrtega <mat-1998-ort@hotmail.com>
2025-01-24 17:52:11 -0300

    Refactor StudentItem component; update props to include class and improve data display

--- src/components/list/listItems/StudentItem.tsx
+++ src/components/list/listItems/StudentItem.tsx
@@ -1,27 +1,18 @@
 import FormModal from "@/components/FormModal";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { role } from "@/constants/data";
+import { Class, Student } from "@prisma/client";
 import { EditIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 
-type Student = {
-  id: number;
-  studentId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone?: string;
-  grade: number;
-  class: string;
-  address: string;
-};
+type StudentItemProps = Student & { class: Class };
 
-function StudentItem(student: Student) {
+function StudentItem(student: StudentItemProps) {
   return (
     <TableRow key={student.id}>
       <TableCell className="flex items-center gap-2">
         <Image
-          src={student.photo}
+          src={student.img || "/avatar-default.png"}
           alt={student.name}
           width={40}
           height={40}
@@ -29,13 +20,13 @@ function StudentItem(student: Student) {
         />
         <div className="flex flex-col">
           <h3 className="font-semibold">{student.name}</h3>
-          <p className="text-xs text-gray-500">{student?.email}</p>
+          <p className="text-xs text-gray-500">{student.class.name}</p>
         </div>
       </TableCell>
+      <TableCell className="hidden md:table-cell">{student.username}</TableCell>
       <TableCell className="hidden md:table-cell">
-        {student.studentId}
+        {student.class.name[0]}
       </TableCell>
-      <TableCell className="hidden md:table-cell">{student.grade}</TableCell>
       <TableCell className="hidden md:table-cell">{student?.phone}</TableCell>
       <TableCell className="hidden lg:table-cell">{student.address}</TableCell>
       <TableCell className="flex items-center gap-2">
